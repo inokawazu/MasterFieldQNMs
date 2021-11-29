fc5b08d95e7298a367100eaab01f5a4511708556
Markus-Amano <markus.a.amano@gmail.com>
2021-11-29 17:54:37 +0900

    added the first part of the horexp function to be

--- src/MasterFieldQNMs.jl
+++ src/MasterFieldQNMs.jl
@@ -10,6 +10,7 @@ bcoef(mfe::MasterFieldEquation) = error("$(mfe) must have an 'b(u,w,q)' coeffice
 horizonlocation(_::MasterFieldEquation{T}) where T = one(T)
 # End MasterFieldEquation
 
+# Begin Transformation functions
 function atrans(a::Function, α::Number)
     return (x...,) -> a(x...) + 2*α
 end 
@@ -17,5 +18,32 @@ end
 function btrans(b::Function, a::Function, α::Number)
     return (x...,) -> b(x...) + a(x...)*α + α*(α-1)
 end 
+# End Transformation functions
+
+
+function horexp(mfe::MasterFieldEquation; hororder = 20)::Function
+            # p; hororder = 20, horloc = 1, transfuncs=[atrans, btrans], ab = [a,b]
+            # T = eltype(p)
+            # w, q = p
+            # α = -im*w/2 # ingoing boundary condition at t  he horizon
+
+            # atrans, btrans = transfuncs
+            # a,b = ab
+            # t = Taylor1(T, horo rder)
+            # ahor = atrans(a, α, t+horloc)(t+horloc, w, q)
+            # bhor = btrans(b,     a, α, t+horloc)(t+horloc, w, q)
+
+            # lhs = 
+            # T[
+            #   (m == n  ? (n-1)*n#=  aₙ=# : 0.0) +
+            #   ((n+1) > m  ? ahor.coeffs[(n+1)-m]*m + bhor.coeffs[(  n+1)-m] #=aₘ=# : 0.0)
+            #   for n in 1:hororder, m in 1:hororder
+            #  ]
+
+            # rh  s = 
+            # T[-bhor.coeffs[n+1] for n in 1:hororder]
+
+            # return Taylor1(T[1;     lhs\rhs])
+        end
 
 end # module
