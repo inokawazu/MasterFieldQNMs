5ee6cf88b9e9a94fbf5c5cc1ea9f41bf8819a644
Markus-Amano <markus.a.amano@gmail.com>
2021-11-29 17:39:17 +0900

    added the MasterFieldEquation abstract type

--- src/MasterFieldQNMs.jl
+++ src/MasterFieldQNMs.jl
@@ -1,4 +1,11 @@
 module MasterFieldQNMs
 
 
+# Begin MasterFieldEquation
+abstract type MasterFieldEquation end
+
+acoef(mfe::MasterFieldEquation) = error("$(mfe) must have an 'a(u,w,q)' coefficent.")
+bcoef(mfe::MasterFieldEquation) = error("$(mfe) must have an 'b(u,w,q)' coefficent.")
+# End MasterFieldEquation
+
 end # module
