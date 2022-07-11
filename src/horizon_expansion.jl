d3e7538c44c66198cb5619678ebaac43d32a9912
Markus-Amano <markus.a.amano@gmail.com>
2022-07-11 13:04:34 +0800

    Added ccoef and fixed horizon expansion.

--- src/horizon_expansion.jl
+++ src/horizon_expansion.jl
@@ -1,6 +1,5 @@
-
 function horexp(
-    mfe::MasterFieldEquation, w::Number, q::Number; hororder = 20
+    mfe::MasterFieldEquation, w::Number, q::Number; hororder::Integer = 20
   )
     w, q = promote(w, q)
     T = real(eltype(w))
@@ -13,6 +12,7 @@ function horexp(
 
     ahor = atrans(a,α)(t+horloc, w, q)
     bhor = btrans(b,a,α)(t+horloc, w, q)
+    chor = ccoef(mfe)(t+horloc, w, q)
 
     out_series = Array{Complex{T}}(undef, hororder)
     out_series[1] = one(Complex{T})
@@ -20,10 +20,14 @@ function horexp(
     for ord in 2:hororder
         m = ord - 1
         s = sum(
-                (bhor.coeffs[ord - k] + (k-1)*ahor.coeffs[ord - k])*out_series[k] 
+                (
+                 bhor.coeffs[ord-(k-1)] + 
+                 (k-1)*ahor.coeffs[ord-(k-1)] + 
+                 (k-2)*(k-1)*chor.coeffs[ord-(k-1)]
+                )*out_series[k] 
                 for k in 1:ord-1
                )
-        out_series[ord] = - s/(m*(m-1) + m*ahor.coeffs[1])
+        out_series[ord] = - s/(m*(m-1)*chor.coeffs[1] + m*ahor.coeffs[1] + bhor.coeffs[1])
     end
 
     return out_series
@@ -58,7 +62,7 @@ end
 
 # TODO: convergence is weak.
 function hubeny_horowitz_qnm(
-    mfe::MasterFieldEquation, w0::Number, q::Number; hororder = 20,
+    mfe::MasterFieldEquation, w0::Number, q::Number; hororder::Integer = 20,
     xatol=nothing, xrtol=nothing, maxevals=1000
     ) 
 
--- src/master_field_equation.jl
+++ src/master_field_equation.jl
@@ -4,6 +4,8 @@ acoef(mfe::MasterFieldEquation) = error("$(mfe) must have an 'a(u,w,q)' coeffice
 
 bcoef(mfe::MasterFieldEquation) = error("$(mfe) must have an 'b(u,w,q)' coefficent.")
 
+ccoef(_::MasterFieldEquation) = (x, _...,) -> one(x)
+
 indicialexponent(mfe::MasterFieldEquation) = error("$(mfe) must have an indicial exponent, α(w,q)")
 
 horizonlocation(_::MasterFieldEquation) = 1
